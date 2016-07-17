31f8fd973c2bff60fb6471005c1604ce4bad3934
Sigmaroot.de <admin@sigmaroot.de>
2016-07-17 23:25:24 +0200

    v_1_3
    UPDATE 17.07.2016

--- localization.lua
+++ localization.lua
@@ -2,9 +2,9 @@
  -- Localization
 
 if(GetLocale() == "enUS") then
-	LOC_STARTUP = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset 1.2 loaded! |cFFFFD000Use /sir to reset your instances";
+	LOC_STARTUP = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset 1.3 loaded! |cFFFFD000Use /sir to reset your instances";
 	LOC_RESET = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset: |cFFFFD000Your Instances have been reset!";
 elseif(GetLocale() == "deDE") then
-	LOC_STARTUP = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset 1.2 geladen! |cFFFFD000Benutze /sir um deine Instanzen zur\195\188ckzusetzen";
+	LOC_STARTUP = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset 1.3 geladen! |cFFFFD000Benutze /sir um deine Instanzen zur\195\188ckzusetzen";
 	LOC_RESET = "|c00FFD700S|c00FFFFFFimple|c00FFD700I|c00FFFFFFnstance|c00FFD700R|c00FFFFFFeset: |cFFFFD000Deine Instanzen wurden zur\195\188ckgesetzt!";
 end
